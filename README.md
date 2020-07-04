a27c094be4c6d6d6c882c1a481d0a20295939c82
Ben White <benwhite2010@gmail.com>
2020-07-03 22:49:52 -0500

    last edit

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 Hello, this is the readMe generator application I built.
+It is not complete, but I psuedo coded as much as I could given the time constraints
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -6,14 +6,14 @@ function generateMarkdown(data) {
   /**
    * the table of contents would be first, with the link for each element within brackets
    * the content of each section would be rendered using the ${} notation and populated with the response object
-   * 
+   * I'm not too worried about the markdown syntax, as I am just happy that I got the function to call and write to a file successfully
    */
   
 
 
    
 
-  `;
+  ;
 }
 // export statement to gain access to the generateMarkdown function
 module.exports = generateMarkdown;
